0d589edb95f95fbb7f860984c8d6d47d1e5d1148
Arnout Boks <arnout.boks@moxio.com>
2020-09-15 16:15:58 +0200

    Refactor DefinitionListParser for easier extension

--- src/DefinitionListParser.php
+++ src/DefinitionListParser.php
@@ -14,36 +14,63 @@ class DefinitionListParser implements BlockParserInterface
             return false;
         }
 
-        $originalContainer = $context->getContainer();
         if ($cursor->peek(0) !== ":") {
             return false;
         }
 
-        if (!($originalContainer instanceof Paragraph || $originalContainer instanceof DefinitionList || $originalContainer instanceof DefinitionListItemDefinition)) {
+        $originalContainer = $context->getContainer();
+        if ($originalContainer instanceof Paragraph) {
+            $originalContainerParent = $originalContainer->parent();
+            if ($originalContainerParent instanceof DefinitionListItemDefinition) {
+                // This paragraph is part of a previous definition, so we shouldn't convert it into terms.
+                // We can start a new definition right away. It will automatically be placed after the current
+                // definition, as a definition block cannot contain another definition block.
+            } else if ($originalContainerParent instanceof DefinitionList) {
+                // Remove the paragraph and turn it into terms within the definition list
+                $originalContainer->detach();
+                $this->switchContextToList($context, $originalContainerParent);
+                $this->addTermsFromParagraph($context, $originalContainer);
+            } else {
+                // Start a new definition list and turn the paragraph into terms within that list
+                $context->replaceContainerBlock(new DefinitionList());
+                $this->addTermsFromParagraph($context, $originalContainer);
+            }
+
+            $this->startDefinition($context, $cursor);
+
+            return true;
+        } else if ($originalContainer instanceof DefinitionList) {
+            $this->startDefinition($context, $cursor);
+
+            return true;
+        } else if ($originalContainer instanceof DefinitionListItemDefinition) {
+            $this->startDefinition($context, $cursor);
+
+            return true;
+        } else {
             return false;
         }
+    }
 
-        if ($originalContainer instanceof Paragraph && !($originalContainer->parent() instanceof DefinitionListItemDefinition)) {
-            $originalContainerParent = $originalContainer->parent();
-            if ($originalContainerParent instanceof DefinitionList) {
-                $originalContainer->detach();
-                $context->setContainer($originalContainerParent);
-                $context->setTip($originalContainerParent);
-            } else {
-                $context->replaceContainerBlock(new DefinitionList());
-            }
+    private function switchContextToList(ContextInterface $context, DefinitionList $definitionList): void
+    {
+        $context->setContainer($definitionList);
+        $context->setTip($definitionList);
+    }
 
-            $strings = $originalContainer->getStrings();
-            foreach ($strings as $string) {
-                $context->addBlock(new DefinitionListItemTerm([ $string ]));
-            }
+    private function addTermsFromParagraph(ContextInterface $context, Paragraph $paragraph): void
+    {
+        foreach ($paragraph->getStrings() as $term) {
+            $context->addBlock(new DefinitionListItemTerm([ $term ]));
         }
+    }
 
+    private function startDefinition(ContextInterface $context, Cursor $cursor): void
+    {
         $cursor->advanceBy(1);
         $cursor->advanceToNextNonSpaceOrTab();
+
         $context->addBlock(new DefinitionListItemDefinition());
         $context->addBlock(new Paragraph());
-
-        return true;
     }
 }
