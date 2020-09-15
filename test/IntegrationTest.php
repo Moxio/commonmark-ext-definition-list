2f07f132c2be0564d062c4fdf324144b549382f6
Arnout Boks <arnout.boks@moxio.com>
2020-09-15 09:28:46 +0200

    Refactored tests to make it easier to add more

--- test/IntegrationTest.php
+++ test/IntegrationTest.php
@@ -9,9 +9,32 @@ use PHPUnit\Framework\TestCase;
 
 class IntegrationTest extends TestCase
 {
-    public function testParsesAndRendersDefinitionList(): void
+    // Example based on https://www.markdownguide.org/extended-syntax#definition-lists
+    public function testParsesAndRendersSimpleDefinitionList(): void
     {
-        $source = <<<MD
+        $markdown = <<<MD
+First Term
+: This is the definition of the first term.
+
+Second Term
+: This is one definition of the second term.
+MD;
+        $expectedHtml = <<<HTML
+<dl>
+  <dt>First Term</dt>
+  <dd>This is the definition of the first term.</dd>
+  <dt>Second Term</dt>
+  <dd>This is one definition of the second term.</dd>
+</dl>
+HTML;
+
+        $this->assertMarkdownIsConvertedTo($expectedHtml, $markdown);
+    }
+
+    // Example from https://www.markdownguide.org/extended-syntax#definition-lists
+    public function testSupportsMultipleDefinitionsForOneTerm(): void
+    {
+        $markdown = <<<MD
 First Term
 : This is the definition of the first term.
 
@@ -19,7 +42,7 @@ Second Term
 : This is one definition of the second term.
 : This is another definition of the second term.
 MD;
-        $expectedOutput = <<<HTML
+        $expectedHtml = <<<HTML
 <dl>
   <dt>First Term</dt>
   <dd>This is the definition of the first term.</dd>
@@ -29,13 +52,18 @@ MD;
 </dl>
 HTML;
 
+        $this->assertMarkdownIsConvertedTo($expectedHtml, $markdown);
+    }
+
+    public function assertMarkdownIsConvertedTo($expectedHtml, $markdown): void
+    {
         $environment = Environment::createCommonMarkEnvironment();
         $environment->addExtension(new DefinitionListExtension());
 
         $parser = new DocParser($environment);
         $renderer = new HtmlRenderer($environment);
-        $actualOutput = $renderer->renderBlock($parser->parse($source));
+        $actualOutput = $renderer->renderBlock($parser->parse($markdown));
 
-        $this->assertXmlStringEqualsXmlString($expectedOutput, $actualOutput);
+        $this->assertXmlStringEqualsXmlString($expectedHtml, $actualOutput);
     }
 }
